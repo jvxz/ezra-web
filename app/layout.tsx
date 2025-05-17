60d3694d296f278bc2a653f5e5ae1096797a4be8
jamie 🌨️ <jamie@wisp.bio>
2025-05-17 19:01:42 -0400

    feat: fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from 'next'
 import { ConvexAuthNextjsServerProvider } from '@convex-dev/auth/nextjs/server'
-import { Geist, Geist_Mono } from 'next/font/google'
 import { Providers } from '@/components/providers'
+import { mono, sans } from '@/lib/font'
 import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -28,7 +18,7 @@ export default function RootLayout({ children }: Readonly<{
   return (
     <ConvexAuthNextjsServerProvider>
       <html lang="en">
-        <body className={`${geistSans.variable}${geistMono.variable} antialiased`}>
+        <body className={`${sans.variable} antialiased ${mono.variable}`}>
           <Providers>{children}</Providers>
         </body>
       </html>
