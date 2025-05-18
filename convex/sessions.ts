582b2e3bf3875cafff4ef571f4417336407628c8
jamie 🌨️ <jamie@wisp.bio>
2025-05-18 16:50:31 -0400

    feat: session functions

--- convex/sessions.ts
+++ convex/sessions.ts
@@ -22,6 +22,25 @@ export const collect = query({
   },
 })
 
+export const pickCurrentSession = query({
+  args: {
+    includeSessionTasks: v.boolean(),
+  },
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx)
+    if (userId === null) {
+      throw new Error('Unauthenticated call to mutation')
+    }
+
+    const currentSession = await ctx.db.query('sessions')
+      .filter(q => q.eq(q.field('userId'), userId))
+      .filter(q => q.eq(q.field('isActive'), true))
+      .first()
+
+    return currentSession
+  },
+})
+
 export const startSession = mutation({
   args: {
   },
@@ -36,6 +55,33 @@ export const startSession = mutation({
       isActive: true,
       id: crypto.randomUUID(),
       start: Date.now(),
+    })
+
+    return session
+  },
+})
+
+export const endSession = mutation({
+  args: {
+  },
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx)
+    if (userId === null) {
+      throw new Error('Unauthenticated call to mutation')
+    }
+
+    const session = await ctx.db.query('sessions')
+      .filter(q => q.eq(q.field('userId'), userId))
+      .filter(q => q.eq(q.field('isActive'), true))
+      .first()
+
+    if (session === null) {
+      throw new Error('No active session found')
+    }
+
+    await ctx.db.patch(session._id, {
+      end: Date.now(),
+      isActive: false,
     })
 
     return session
