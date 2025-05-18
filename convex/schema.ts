226a096e2c2b76b3d361f7be6be4c14efcedf9fe
jamie 🌨️ <jamie@wisp.bio>
2025-05-18 16:09:01 -0400

    feat: convex schemas

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,12 +2,31 @@ import { authTables } from '@convex-dev/auth/server'
 import { defineSchema, defineTable } from 'convex/server'
 import { v } from 'convex/values'
 
+const sessionSchema = defineTable({
+  id: v.string(),
+  userId: v.id('users'),
+  isActive: v.boolean(),
+  start: v.number(),
+  end: v.optional(v.number()),
+})
+
+const taskSchema = defineTable({
+  id: v.string(),
+  userId: v.id('users'),
+  sessionId: v.id('sessions'),
+  isActive: v.boolean(),
+  start: v.number(),
+  end: v.optional(v.number()),
+  duration: v.optional(v.number()),
+  aet: v.number(),
+  description: v.string(),
+})
+
 // The schema is normally optional, but Convex Auth
 // requires indexes defined on `authTables`.
 // The schema provides more precise TypeScript types.
 export default defineSchema({
   ...authTables,
-  numbers: defineTable({
-    value: v.number(),
-  }),
+  sessions: sessionSchema,
+  tasks: taskSchema,
 })
