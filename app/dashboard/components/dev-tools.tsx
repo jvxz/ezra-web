3b6eec00928667b98383b3947e74162ca5b23ffc
jamie 🌨️ <jamie@wisp.bio>
2025-05-19 13:55:18 -0400

    feat: dev tools

--- app/dashboard/components/dev-tools.tsx
+++ app/dashboard/components/dev-tools.tsx
@@ -1,23 +1,72 @@
-import { convexAuthNextjsToken } from '@convex-dev/auth/nextjs/server'
-import { fetchMutation } from 'convex/nextjs'
+'use client'
+import type { Id } from '@/convex/_generated/dataModel'
+import { useMutation, useQuery } from 'convex/react'
 import { Button } from '@/components/ui/button'
 import { api } from '@/convex/_generated/api'
 
-async function DevTools() {
-  const token = await convexAuthNextjsToken()
+function DevTools() {
+  const currentSession = useQuery(api.sessions.pickCurrentSession, {
+    includeSessionTasks: false,
+  })
+
+  const handleStartSession = useMutation(api.sessions.startSession).withOptimisticUpdate((local) => {
+    const allSessions = local.getQuery(api.sessions.collect, {
+      includeActiveSession: true,
+    })
+
+    if (!allSessions) return
+
+    local.setQuery(api.sessions.collect, {
+      includeActiveSession: true,
+    }, [
+      ...allSessions,
+      {
+        _creationTime: Date.now(),
+        _id: 'new' as Id<'sessions'>,
+        id: 'new',
+        userId: 'new' as Id<'users'>,
+        isActive: true,
+        start: Date.now(),
+      },
+    ])
+  })
+
+  const handleEndSession = useMutation(api.sessions.endSession).withOptimisticUpdate((local) => {
+    const allSessions = local.getQuery(api.sessions.collect, {
+      includeActiveSession: true,
+    })
+
+    if (!allSessions) return
+
+    const activeSession = allSessions.find(session => session.isActive)
+    if (!activeSession) return
+
+    local.setQuery(api.sessions.collect, {
+      includeActiveSession: true,
+    }, [
+      ...allSessions.filter(s => s._id !== activeSession._id),
+      {
+        ...activeSession,
+        isActive: false,
+        // end: Date.now(),
+      },
+    ])
+  })
+
   return (
     <div className="fixed right-12 bottom-12">
       <div className="flex items-center gap-2">
         <Button
+          disabled={currentSession?.isActive}
           variant="outline"
-          onClick={async () => {
-            'use server'
-            void fetchMutation(api.sessions.startSession, {
-            }, {
-              token,
-            })
-          }}
+          onClick={() => handleStartSession()}
         >start session
+        </Button>
+        <Button
+          disabled={!currentSession?.isActive}
+          variant="outline"
+          onClick={() => handleEndSession()}
+        >end session
         </Button>
       </div>
     </div>
